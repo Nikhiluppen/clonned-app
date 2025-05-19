34881eb052d26b02ef2b6463252b0e9486186a74
Nikhiluppen <nikhiluppen1654@gmail.com>
2025-05-19 23:38:04 +0000

    addflies

--- src/App.js
+++ src/App.js
@@ -14,3 +14,20 @@ function App() {
 // w,fhywbgflwehfulhidhckjdhwluihlciQWDS  QS  S QS        QQwhdiuwegfygwylwewd322e2edd2dbaskhbj;ohiuh;oihuhuigig
 
 export default App;
+
+// import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       hello
+//       <Home />
+      
+//     </div>
+//   );
+// }
+// // w,fhywbgflwehfulhidhckjdhwluihlciQWDS  QS  S QS        QQwhdiuwegfygwylwewd322e2edd2dbaskhbj;ohiuh;oihuhuigig
+
+// export default App;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -55,3 +55,15 @@ const Product = ({ data = {} }) => {
 
 export default Product;
 
+function App() {
+  return (
+    <div className="App">
+      hello
+      <Home />
+      
+    </div>
+  );
+}
+// w,fhywbgflwehfulhidhckjdhwluihlciQWDS  QS  S QS        QQwhdiuwegfygwylwewd322e2edd2dbaskhbj;ohiuh;oihuhuigig
+
+export default App;
