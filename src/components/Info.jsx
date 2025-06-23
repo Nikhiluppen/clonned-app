f03f5f881e36844e6ddca30f90d44f2ce30c30ac
Nikhiluppen <nikhiluppen1654@gmail.com>
2025-06-23 23:20:28 +0000

    aboutpage

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -113,3 +113,15 @@ const Section = ({ data = {} }) => {
 export default Section;
 
 
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '2rem', color: '#333' }}>
+      <h1>About Us</h1>
+      <p>This is the About page. Learn more about what we do!</p>
+    </div>
+  );
+};
+
+export default About;
