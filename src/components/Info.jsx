df200bbbad9633d8674c8bc70c7d5f1234ff6a4a
Nikhiluppen <nikhiluppen1654@gmail.com>
2025-07-13 23:33:17 +0000

    cloned

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -138,180 +138,61 @@ const Header = () => {
         )
         // fafhvmhfg,myftkdsFDBHSBVDKJBVdqwKJGBHJKGBH,GKYHLhvmgjh,ygguYWSQWSDQsxadwfiqwddqsd    cqsfdgafwfc;io   jwqdc   qj qjeifj   ;ojqi'  ji'jfERGFBHFGEYLA efnuefhuehf jhjlvf rfjhefwio fhefnefufhlwebnlc ruf
     }
-export default Header;
+eimport { Box, Typography, styled } from '@mui/material';
 
-   import { Typography, Box, styled } from "@mui/material";
-
+// Styled Components
 const Component = styled(Box)({
-  display: "flex",
-  justifyContent: "center",
-  padding: "12px 0",
-  color: "#1d1d1f",
-  fontSize: "14px",
-  alignItems: "center",
-  height: "44px",
-});
-
-const Info = () => {
-  return (
-    <Component>
-      <Typography>
-        Get the latest information on Apple products and services, including news,
-        updates, and more. Shop now to get great deals and enjoy free shipping
-        on orders over $50 of selected items.*{" "}
-        <span style={{ color: "#2997ff" }}>Shop now &gt;</span>
-      </Typography>
-    </Component>
-  );
-};
-
-export default Info;
-
-import { Box, Typography, styled } from "@mui/material";
-
-const Component = styled(Box)({
-  width: '100%',
-  height: 580,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  paddingTop: 47,
-  objectFit: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
-});
-
-const Heading = styled(Typography)({
-  fontWeight: 600,
-  fontSize: 56
-});
-
-const SubHeading = styled(Typography)({
-  fontSize: 28,
-  padding: '0 0 5px 0'
-});
-
-const linkStyle = {
-  color: '#2997ff',
-  padding: '0 30px',
-  fontSize: 21,
-  cursor: 'pointer'
-};
-
-const Section = ({ data = {} }) => {
-  const {
-    imageURL = '',
-    heading = '',
-    headColor = '#000',
-    subHeading = '',
-    subHeadColor = '#333'
-  } = data;
-
-  return (
-    <Component style={{ background: `url(${imageURL}) 50% 50% no-repeat` }}>
-      <Heading style={{ color: headColor }}>{heading}</Heading>
-      <SubHeading style={{ color: subHeadColor }}>{subHeading}</SubHeading>
-      <Typography>
-        <Typography component="span" style={linkStyle}>Learn more &gt;</Typography>
-        <Typography component="span" style={linkStyle}>Buy &gt;</Typography>
-      </Typography>
-    </Component>
-  );
-};
-
-export default Section;
-
-import Box from '@mui/material/Box';
-import { Typography , styled } from '@mui/material';
-
-
-import SearchIcon from '@mui/icons-material/Search';
-import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
-import { applelogo,navData } from '../constants/constant';
-// import { Opacity } from '@mui/icons-material';  
-
-
-
-const Component = styled(Box) ({
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: '10px 20px',
-    background: '#f5f5f5',
-    color: '#000',
-    alignItems: 'center',
-    height: '44px',
-    color: '#000',
-    alignItems: 'center',
-    height: '44px'
-}) ;
-
-const NavBar = styled(Box) ({
-    display: 'flex',
     width: '100%',
-    justifyContent: 'center',
-    // background: '#f5f5f5',
+    height: 580,
+    display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
-    marginLeft: '0 auto',
-    '& > *': {
-        marginRight: '20px',
-        fontSize: '12px',
-        padding: '0 21px',
-        Opacity: 0.8,
-        // fontWeight: 600,
-        color: '#000'
-    }
-});
-
-const Header = () => {
-    return (
-        <Component>
-            <NavBar>
-                <img src={applelogo} alt="logo"  style={{width: 20}}/>
-                {
-                    navData.map(nav  => (
-                        <Typography> {nav}</Typography>
-                       
-                    ))  
-                }
-                <SearchIcon  fontSize='small'/>
-                <WorkOutlineIcon fontSize='small' />
-            </NavBar>
-        </Component>
-        )
-        // fafhvmhfg,myftkdsFDBHSBVDKJBVdqwKJGBHJKGBH,GKYHLhvmgjh,ygguYWSQWSDQsxadwfiqwddqsd    cqsfdgafwfc;io   jwqdc   qj qjeifj   ;ojqi'  ji'jfERGFBHFGEYLA efnuefhuehf jhjlvf rfjhefwio fhefnefufhlwebnlc ruf
-    }
-export default Header;
-
-   import { Box, Typography, styled } from "@mui/material";
-
-const Component = styled(Box)({
-  width: '100%',
-  height: 580,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  paddingTop: 47,
-  objectFit: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
+    paddingTop: 47,
+    objectFit: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
 });
 
 const Heading = styled(Typography)({
-  fontWeight: 600,
-  fontSize: 56
+    fontWeight: 600,
+    fontSize: 56
 });
 
 const SubHeading = styled(Typography)({
-  fontSize: 28,
-  padding: '0 0 5px 0'
+    fontSize: 28,
+    padding: '0 0 5px 0'
 });
 
 const linkStyle = {
-  color: '#2997ff',
-  padding: '0 30px',
-  fontSize: 21,
-  cursor: 'pointer'
+    color: '#2997ff',
+    padding: '0 30px',
+    fontSize: 21,
+    cursor: 'pointer'
 };
+
+// Functional ComponentwedqwQWD DXWQDDdqwsqdqdqwdqc   wdwdQDQWDQW,HGGF,YGYG,ILYGsaEWD
+const Product = ({ data = {} }) => {
+    const {
+        imageURL = '',
+        heading = '',
+        headColor = '#000',
+        subHeading = '',
+        subHeadColor = '#333'
+    } = data;
+
+    return (
+        <Component style={{ background: `url(${imageURL}) 50% 50% no-repeat` }}>
+            <Heading style={{ color: headColor }}>{heading}</Heading>
+            <SubHeading style={{ color: subHeadColor }}>{subHeading}</SubHeading>
+            <Typography>
+                <Typography component="span" style={linkStyle}>Learn more &gt;</Typography>
+                <Typography component="span" style={linkStyle}>Buy &gt;</Typography>
+            </Typography>
+        </Component>
+    );
+};
+// JRBGHDERBFYAGHIHFImyftkdsFDBHSBVDKJBVdqwKJGBHJKGBHmyftkdsFDBHSBVDKJBVdqwKJGBHJKGBHGREYFghghghlasdkfga<Gcyegflge,sjfbcxvhlayergrfwefg,bscghdjfvbahdsjfgeuyiwg.jkb;hu;hlh bgugh;
+
 
 const Section = ({ data = {} }) => {
   const {
@@ -336,3 +217,4 @@ const Section = ({ data = {} }) => {
 
 export default Section;
 import { Box, Typography, styled } from '@mui/material';
+
