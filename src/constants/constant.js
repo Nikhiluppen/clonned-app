b033a42636b4befa9444cf250a838c890f543dce
Nikhil Ega <nikhiluppen1654@gmail.com>
2025-12-14 01:35:37 +0000

    edit

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -71,4 +71,79 @@ export const products = {
     }
 }
 
-// HV,HG,GYG    DSasasc  DDqwfqwdwfwfwfwd  G,KTU
+// HV,HG,GYG    DSasasc  DDqwfqwdwfwfwfwd  G,KTUconst linkStyle = {
+  color: '#2997ff',
+  padding: '0 30px',
+  fontSize: 21,
+  cursor: 'pointer',
+};
+
+const Section = ({ data = {} }) => {
+  const {
+    imageURL = '',
+ import { Typography, Box, styled } from "@mui/material";
+ 
+ const Component = styled(Box)({
+   display: "flex",
+   justifyContent: "center",
+   padding: "12px 0",
+   color: "#1d1d1f",
+   fontSize: "14px",
+   alignItems: "center",
+   height: "44px",
+ });
+ 
+ const Info = () => {
+   return (
+     <Component>
+       <Typography>
+         Get the latest information on Apple products and services, including news,
+         updates, and more. Shop now to get great deals and enjoy free shipping
+         on orders over $50 of selected items.*{" "}
+         <span style={{ color: "#2997ff" }}>Shop now &gt;</span>
+       </Typography>
+     </Component>
+   );
+ };
+ 
+ export default Info;
+
+export default About;
+const linkStyle = {
+  color: '#2997ff',
+  padding: '0 30px',
+  fontSize: 21,
+  cursor: 'pointer',
+};
+
+const Section = ({ data = {} }) => {
+  const {
+    imageURL = '',
+ import { Typography, Box, styled } from "@mui/material";
+ 
+ const Component = styled(Box)({
+   display: "flex",
+   justifyContent: "center",
+   padding: "12px 0",
+   color: "#1d1d1f",
+   fontSize: "14px",
+   alignItems: "center",
+   height: "44px",
+ });
+ 
+ const Info = () => {
+   return (
+     <Component>
+       <Typography>
+         Get the latest information on Apple products and services, including news,
+         updates, and more. Shop now to get great deals and enjoy free shipping
+         on orders over $50 of selected items.*{" "}
+         <span style={{ color: "#2997ff" }}>Shop now &gt;</span>
+       </Typography>
+     </Component>
+   );
+ };
+ 
+ export default Info;
+
+export default About;
