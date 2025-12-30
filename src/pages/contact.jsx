316bb21ea6cfe93403db76e94f49b5616f7c8782
Nikhil Ega <nikhiluppen1654@gmail.com>
2025-12-30 05:22:10 +0000

    uihhi

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -340,3 +340,42 @@ const About = () => {
 };
 
 export default About;
+export default About;
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '2rem', color: '#333' }}>
+      <h1>About Us</h1>
+      <p>This is the About page. Learn more about what we do!</p>
+    </div>
+  );
+};
+
+export default About;
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '2rem', color: '#333' }}>
+      <h1>About Us</h1>
+      <p>This is the About page. Learn more about what we do!</p>
+    </div>
+  );
+};
+
+export default About;
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '2rem', color: '#333' }}>
+      <h1>About Us</h1>
+      <p>This is the About page. Learn more about what we do!</p>
+    </div>
+  );
+};
+
+export default About;
