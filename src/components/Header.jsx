55de6d96bed1c1ea632559f35d761df5e17a944a
Nikhil Ega <nikhiluppen1654@gmail.com>
2026-01-20 00:59:02 +0000

    EID

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -948,7 +948,27 @@ const About = () => {
       );
       );v
     export default About;
-    
+     
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return (
+          <div style={{ padding: '2rem', color: '#333' }}>
+            <h1>About Us</h1>
+            <p>This is the About page. Learn more about what we do!</p>
+          </div>
+        );
+        );
+        );
+        );v
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return (
     import React from 'react';
     
     const About = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -407,4 +407,64 @@ const About = () => {
       import React from 'react';
       
       const About = () => {
-        return (s)
+        return (s) 
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return (
+          <div style={{ padding: '2rem', color: '#333' }}>
+            <h1>About Us</h1>
+            <p>This is the About page. Learn more about what we do!</p>
+          </div>
+        );
+        );
+        );
+        );v
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return ( 
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return (
+          <div style={{ padding: '2rem', color: '#333' }}>
+            <h1>About Us</h1>
+            <p>This is the About page. Learn more about what we do!</p>
+          </div>
+        );
+        );
+        );
+        );v
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return ( 
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return (
+          <div style={{ padding: '2rem', color: '#333' }}>
+            <h1>About Us</h1>
+            <p>This is the About page. Learn more about what we do!</p>
+          </div>
+        );
+        );
+        );
+        );v
+      export default About;
+      
+      import React from 'react';
+      
+      const About = () => {
+        return (
